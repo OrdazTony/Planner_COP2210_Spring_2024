052e6d981d6fd7838d3ec89a682a2dcc319546d6
anthonyordaz <ordaz.tony.24@gmail.com>
2024-11-07 13:22:23 -0500

    New folder named 'Program' encasing  Main.java file and subsequent files added for project functionality.

---
+++ Program/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*Here we will have our Main program running. This is what will be known as the interface.
+        * Function calls and a menu will be present here.
+        * Users should be able to interact with the interface to leave reminders or plan their day.
+        * interface will hopefully be interactive.
+        *
+        *
+        * */
+        }
+    }
